b41b350a447321e5191e414a0643c164d6379c83
Hong Nhat Pham <hongnhat.ph@gmail.com>
2022-04-30 14:44:39 +0700

    Added installation programs

--- install.sh
+++ install.sh
@@ -1,1 +1,53 @@
+#!/bin/bash
 
+#Update & upgrade
+sudo apt update && sudo apt upgrade -y
+
+#Install git
+sudo apt install git -y
+
+#Ly Lock screen
+sudo apt install build-essential libpam0g-dev libxcb-xkb-dev -y
+
+cd ~
+git clone --recurse-submodules https://github.com/nullgemm/ly.git
+cd ly
+make
+sudo make run
+sudo make install
+sudo systemctl enable ly.service
+cd ~
+rm -r ./ly
+
+#Install Xorg display server
+sudo apt install xorg xserver-xorg xutils mesa-utils xinit arandr autorandr -y
+
+#Install firmwares
+sudo apt install firmware-linux firmware-linux-nonfree -y
+
+#Install terminal programs
+sudo apt install vim neovim htop -y
+
+#Install power management
+cd ~
+git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+cd auto-cpufreq && sudo ./auto-cpufreq-installer
+
+sudo auto-cpufreq --install
+
+
+#Install fonts
+sudo apt install ttf-mscorefonts-installer -y
+
+#Install desktop environment
+sudo apt install spectrwm dunst lxappearance xterm nitrogen pcmanfm rofi -y
+
+#Brave browser
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser -y
+
+#Install other desktop programs
+sudo apt install kdeconnect -y
